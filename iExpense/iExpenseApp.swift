7c7d29704a0d6c27da66bc698ebb6206b573c79a
Eliezer Sant Ana <eliezer.ana@f1rst.com.br>
2024-07-21 21:59:08 -0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Eliezer Rodrigo Beltramin de Sant Ana on 21/07/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
